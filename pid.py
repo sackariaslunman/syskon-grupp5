c33a1c5c83367f161055ec0024edea33edabcccd
Sackarias Lunman <lunman@student.chalmers.se>
2022-11-10 14:30:39 +0100

    created pid controller class

--- pid.py
+++ pid.py
@@ -1,9 +1,18 @@
 class PID:
-    def __init__(self, Kp = 0, Ki = 0, Kd = 0):
+    def __init__(self, Kp = 0, Ki = 0, Kd = 0, h = 0.001, goal = 0):
         self.Kp = Kp
         self.Ki = Ki
         self.Kd = Kd
-        self.e = []
+        self.u = 0
+        self.u0 = 0
+        self.h = h
+        self.goal = goal
 
-    def update(self, error):
-        self.e.append = 
+    def update(self, input):
+        error = self.goal - input
+        P = self.Kp * error
+        I = self.u + self.Ki * self.h * error
+        D = self.Kd * (self.u - self.u0) / self.h
+        self.u0 = self.u
+        self.u = P + I + D
+        return self.u
