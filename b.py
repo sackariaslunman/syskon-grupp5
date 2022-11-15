ae47866aff73ae95ae1be034b997c93a6baac0ee
Znarf505 <90909089+Znarf505@users.noreply.github.com>
2022-11-15 12:58:04 +0100

    Nu har vi en global_variabel fil med allt som behövs. Det ger samma resultat som det gjorde innan.

--- b.py
+++ b.py
@@ -1,176 +1,113 @@
 import numpy as np
 import math
 from matplotlib import pyplot as plt
+import global_var as gl
 
-#motorns variabler
-maxTDevMotor = 0.5 #[Nm]
-maxWmotor = 502.4 #[w] rad/s
-maxEffekt = 251.2 #[w] watt
-maxU = 10.67 #[U] spänning
-maxI = 28.8 # [i] ström
-resistans = 0.0459 # [R] resistans
-spänningskonstant = 0.0184
-utväxling = 3.5**5 # 1 : 525.2
-förluster = 0.95**5
+"""
+Simulering och tidssteg osv: 
+# """
 
-#Ytterliggare variabler
-MLast = 1500
-g = 9.82 #gravitation
-#maxTLast = 189.88 # [Nm]
-w_last_ref = 0.733 # [w] rad/s
-distance = 6.6 # [m] meter
-maxTime = 180 #[s] sekunder
-#vinschRadie = 0.05 # [m] meter
-vajer_dist = 8. # [m] meter
-
-#Uträknade siffror
-#TDev = maxTLast/(utväxling * förluster) #0.4672 Nm
-#wMotor = wVinsch * utväxling # [w] 385 rad/s
-tyngdkraften = MLast * g * math.sin(12*2*math.pi/360) #vertikaltlyft
-cr = 0.05 # Friktionskonstanten
-friktionskraften = cr * MLast * g * math.cos(12*2*math.pi/360) #normalkraften
-vLast = distance*maxTime
-
-
-#modell av lasten
-#FLast = tyngdkraften + friktionskraften + accelerationskraften
-#PLast = vLast*FLast
-
-#Inparametrar
-
-dt = 0.01 #[s], sekunder per tidssteg för simulering
-T = 500.0 # [s], sekunder för hela simuleringen
-N = round (T/dt) #antal steg att simulera avrundat till heltal
-
-v_last = np.zeros(N) #hastighet på vinsch - begynelsevärde
-a_last = np.zeros(N) #acceleration på vinsch - begynelsevärd
-s_last = np.zeros(N) #sträckan på last - begynelsevärde
-spärr = bool # det som bestämmer om systemet skall stanna eller ej. Bestäms genom trycksensor. 
-
-F_last = np.zeros(N)
-w_last = np.zeros(N)
-w_motor = np.zeros(N)
-T_l = np.zeros(N)
-T_dev = np.zeros(N)
-v_last = np.zeros(N)
-w_last = np.zeros(N)
-w_motor = np.zeros(N)
-P_motor = np.zeros(N)
-I_motor = np.zeros(N)
-U_motor = np.zeros(N)
-P_batt = np.zeros(N)
-I_batt = np.zeros(N)
-U_batt = 12 # [v] Volt - konstant
-W_batt = np.zeros(N)
-r_ref = 0.073   #referenssignal till systemet. Denna ska ändras beroende på vilket steg vi är i simuleringen. Baklänges / framlänges / stopp etc. 
-v_ref = 0.0366
-
-t = np.arange(0., 500., dt)
-
-
-# Oklart vad J ska vara
-J = 0.1
+t = np.arange(0., gl.T, gl.dt)
 
 from pid import PID
 
 def main():
     spärr = False
-    #F_last =(tyngdkraften - friktionskraften) * 1       # faktorn representerar 100 %. 
-    pid = PID(0.1, 1, 0, dt, (maxWmotor/utväxling), -(maxWmotor/utväxling))        # skapar regulatorn med konstanterna Kp = 0.005, Ki = 0.0002, Kd = 0.001 och max min
-                                                                # Elmotorns max rotations 
+    pid = PID(gl.Kp, gl.Ki, gl.dt, (gl.maxwmotor/gl.k), -(gl.maxwmotor/gl.k)) # skapar regulatorn med konstanterna Kp = 0.005, Ki = 0.0002, Kd = 0.001 och max min
+                                                                              # Elmotorns max rotations 
     tryck = 0
-    v_ref = 0.0366
-    for i in range(N):
-        accelerationskraften = a_last[i]*MLast #accelerations kraften utanför för det är samma i alla lägen
-        vinschRadie = 0.05*(vajer_dist/(vajer_dist + (s_last[i-1]*(1/3)))) # Funktion för vinschradiens förhållande till båtens position. (Förklaras i 'a' i rapport)
+    gl.v_ref = 0.0366
+    for i in range(gl.N):
+        F_a = gl.a_last[i]*gl.m_last #accelerations kraften utanför för det är samma i alla lägen
+        r_vinsch = 0.05*(gl.l_vajer/(gl.l_vajer + (gl.s_last[i-1]*(1/3)))) # Funktion för vinschradiens förhållande till båtens position. (Förklaras i 'a' i rapport)
         
-        if(s_last[i-1] < 6 and v_ref > 0): # Båten är på trailern och åker ner mot vattnet
-            v_ref = 0.0366
-            F_last[i] =(tyngdkraften - friktionskraften + accelerationskraften)
+        if(gl.s_last[i-1] < 6 and gl.v_ref > 0): # Båten är på trailern och åker ner mot vattnet
+            gl.v_ref = 0.0366
+            gl.F_last[i] =(gl.F - gl.F_f + F_a)
 
-        elif(s_last[i-1] >= 6 and s_last[i-1] <= 8 and v_ref >0): # Båten åker ut i vattnet i 2 meter
-            v_ref= 0.0366
-            F_last[i] =(accelerationskraften/math.cos(12))
+        elif(gl.s_last[i-1] >= 6 and gl.s_last[i-1] <= 8 and gl.v_ref >0): # Båten åker ut i vattnet i 2 meter
+            gl.v_ref= 0.0366
+            gl.F_last[i] =(F_a/math.cos(12))
 
-        elif((s_last[i-1] > 8 and v_ref > 0) or (s_last[i-1] > 6 and v_ref < 0)): # Båten vänder riktning i vattnet och åker mot trailer
+        elif((gl.s_last[i-1] > 8 and gl.v_ref > 0) or (gl.s_last[i-1] > 6 and gl.v_ref < 0)): # Båten vänder riktning i vattnet och åker mot trailer
             
-            v_ref = -0.0366
-            F_last[i] =(accelerationskraften/math.cos(12))
+            gl.v_ref = -0.0366
+            gl.F_last[i] =(F_a/math.cos(12))
 
         else: #Båten är på trailern och dras upp
-            F_last[i] =(tyngdkraften + friktionskraften + accelerationskraften)
+            gl.F_last[i] =(gl.F + gl.F_f + F_a)
 
-            if tryck > 100 or s_last[i-1] < 0.1: # [Pa] om det är 10 cm kvar på så bromsar den in
-                v_ref = 0
-                I_motor[i] = 0
+            if tryck > 100 or gl.s_last[i-1] < 0.1: # [Pa] om det är 10 cm kvar på så bromsar den in
+                gl.v_ref = 0
+                gl.I_motor[i] = 0
 
             else: # annars så drar den upp båten som vanligt
-                v_ref = -0.0366
+                gl.v_ref = -0.0366
             
 
         if spärr == True: # om spärren är på
-            v_ref = 0
-            I_motor[i] = 0
+            gl.v_ref = 0
+            gl.I_motor[i] = 0
 
 
-        T_l[i] = F_last[i]*vinschRadie
-        T_dev[i] = (T_l[i]/(utväxling * förluster))
+        gl.T_l[i] = gl.F_last[i]*r_vinsch 
+        gl.T_dev[i] = (gl.T_l[i]/(gl.k * gl.f))
 
         if spärr == False:
-            I_motor[i] = T_dev[i]/spänningskonstant
-        if T_dev[i]/spänningskonstant > maxI: 
+            gl.I_motor[i] = gl.T_dev[i]/gl.Ke
+        if gl.T_dev[i]/gl.Ke > gl.maxI: 
             # Om systemet vill få mer ström än vad elmotorn klarar av så stängs systemet av
             spärr = True
         
 
-        w_motor[i] = (U_motor[i] - resistans*I_motor[i])/spänningskonstant
+        gl.w_motor[i] = (gl.U_motor[i] - gl.R*gl.I_motor[i])/gl.Ke
         # Här tar vi ut spänningen och strömmen från systemet med sensorer
         # och sen använder vi dessa värden för att göra en virituell sensor för att hålla koll på hastigheten.
         
-        w_last[i] = w_motor[i]/utväxling
-        v_last[i] = w_last[i]*vinschRadie
+        gl.w_last[i] = gl.w_motor[i]/gl.k
+        gl.v_last[i] = gl.w_last[i]*r_vinsch
 
         
-        P_batt[i] = I_motor[i]*U_motor[i]
-        P_motor[i] = T_dev[i]*w_motor[i]
-        I_batt[i] = P_batt[i]/U_batt
+        gl.P_batt[i] = gl.I_motor[i]*gl.U_motor[i]
+        gl.P_motor[i] = gl.T_dev[i]*gl.w_motor[i]
+        gl.I_batt[i] = gl.P_batt[i]/gl.U_batt
         
-        if i < N-1:
-            U_motor[i+1] = ((pid.update(v_last[i-1], v_ref)/vinschRadie)*utväxling)*spänningskonstant + resistans*I_motor[i] # Får ut fel värde just nu, vi vill få spänning men får v_ref istället
-            if U_motor[i+1] > maxU:
-                U_motor[i+1] = maxU
-            elif U_motor[i+1] < -maxU:
-                U_motor[i+1] = -maxU
+        if i < gl.N-1:
+            gl.U_motor[i+1] = ((pid.update(gl.v_last[i-1], gl.v_ref)/r_vinsch)*gl.k)*gl.Ke + gl.R*gl.I_motor[i] # Får ut fel värde just nu, vi vill få spänning men får v_ref istället
+            if gl.U_motor[i+1] > gl.maxU:
+                gl.U_motor[i+1] = gl.maxU
+            elif gl.U_motor[i+1] < -gl.maxU:
+                gl.U_motor[i+1] = -gl.maxU
 
         if i > 0: #Eulers metod
-            a_last[i] = (v_last[i] - v_last[i-1]) / dt
-            s_last[i] = s_last[i-1] + dt*v_last[i]
-            W_batt[i] = W_batt[i-1] + dt*P_batt[i]
+            gl.a_last[i] = (gl.v_last[i] - gl.v_last[i-1]) / gl.dt
+            gl.s_last[i] = gl.s_last[i-1] + gl.dt*gl.v_last[i]
+            gl.W_batt[i] = gl.W_batt[i-1] + gl.dt*gl.P_batt[i]
 
 
     plt.figure(1)
-    plt.plot(t, v_last)
+    plt.plot(t, gl.v_last)
     plt.xlabel('tid')
     plt.ylabel('hastighet [m/s]')
 
     plt.figure(2)
-    plt.plot(t, s_last)
+    plt.plot(t, gl.s_last)
     plt.xlabel('tid')
     plt.ylabel('distans [m]')
     
     plt.figure(3)
-    plt.plot(t, a_last)
+    plt.plot(t, gl.a_last)
     plt.xlabel('tid')
     plt.ylabel('acceleration [m/s^2]')
 
     
     plt.figure(4)
-    plt.plot(t, I_motor)
+    plt.plot(t, gl.I_motor)
     plt.xlabel('tid')
     plt.ylabel('ström [A]')
 
     plt.figure(5)
-    plt.plot(t, U_motor)
+    plt.plot(t, gl.U_motor)
     plt.xlabel('tid')
     plt.ylabel('spänning [V]')
     
---
+++ global_var.py
@@ -0,0 +1,66 @@
+import math
+import numpy as np
+from matplotlib import pyplot as plt
+
+"""
+Motorns värden: 
+"""
+maxT_dev = 0.5      # [Nm]  Högsta utvecklade vridmoment. 
+maxwmotor = 502.4   # [rad/s] Högsta vinkelhastigheten hos motorn. 
+maxP = 251.2        # [W] Max utvecklad effekt
+maxU = 10.67        # [V] Max spänning ur motorn
+maxI = 28.8         # [I] Max ström genom motorn. 
+R = 0.0459          # [Ohm] Inre resistans hos motorn. 
+Ke = 0.0184         # Spänningskonstanten i motorn. Antas vara samma som Ki. 
+
+"""
+Växellådans värden:
+"""
+k = 3.5**5          # 1 : 525.2  i uväxling.
+f = 0.95**5         # Förluster i växellådan. 5 % per steg. 
+
+"""
+Trailer och last (gravitation med): 
+"""
+m_last = 1500
+g = 9.82            # [m/s^2] gravitationskonstanten.
+w_last_ref = 0.733  # [w] rad/s
+s_total = 6.6       # [m] Meter vilket är den totala sträckan båten kommer färdas i. 
+simulation_duration = 180 # [s] sekunder
+l_vajer = 8.        # [m] Meter. 
+
+
+"""
+Beräknade värden: 
+"""
+F = m_last * g * math.sin(12*2*math.pi/360) #vertikaltlyft
+cr = 0.05 # Friktionskonstanten
+F_f = cr * m_last * g * math.cos(12*2*math.pi/360)       # Friktionskraften. 
+v_last = s_total*simulation_duration
+
+
+dt = 0.01               # [s], sekunder per tidssteg för simulering
+T = 500.0               # [s], sekunder för hela simuleringen
+N = round (T/dt)        # antal steg att simulera avrundat till heltal
+
+
+v_last = np.zeros(N)    # [m/s]     Hastighet på vinsch - begynelsevärde
+a_last = np.zeros(N)    # [m/s^2]   Acceleration på vinsch - begynelsevärd
+s_last = np.zeros(N)    # [m]       Sträckan på last - begynelsevärde
+spärr = bool # det som bes[] tämmer om systemet skall stanna eller ej. Bestäms genom trycksensor. 
+F_last = np.zeros(N)    # [N]       Kraften lasten verkar med på systemet / vinschen. 
+w_motor = np.zeros(N)   # [rad/s]   Rotationshastigheten motorn.
+T_l = np.zeros(N)       # [Nm]      Det utvecklade momentet från systemet och lsaten på vinschen. 
+T_dev = np.zeros(N)     # [Nm]      Motorns utvecklade moment. 
+w_motor = np.zeros(N)
+w_last = np.zeros(N)    # [rad/s]   Rotationshastigheten vinschen
+P_motor = np.zeros(N)   # [W]       Effekt ur motorn. 
+I_motor = np.zeros(N)   # [A]       Ström genom motorn
+U_motor = np.zeros(N)   # [V]       Spänning över motorn 
+P_batt = np.zeros(N)    # [W]       Effekten som batteriet förser
+W_batt = np.zeros(N)    # [W]       Effekt av något. 
+I_batt = np.zeros(N)    # [A]       Strömmen som batteriet förser
+U_batt = 12             # [V]       Spänningen som batteriet alltid förser. 
+v_ref = 0.0366          # [m/s]     Referenssignalen till PI - regulatorn. 
+Kp = 0.1                # Propotionella konstanten av en regulator. 'P' delen. 
+Ki = 1                  # Integrala konstanten av en regulator. 'I' delen. 
