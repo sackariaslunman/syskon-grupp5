aec0888d564fbb1f64e05a4aaac5dddb9d47f426
Johan <johangust1@gmail.com>
2022-11-18 09:32:04 +0100

    lagt till brus i sensor samt fixat spänningsregulatorn på Lars sätt

--- d.py
+++ d.py
@@ -23,22 +23,27 @@ def main():
     for i in range(N):
         F_a = a_last[i]*m_last #accelerations kraften utanför för det är samma i alla lägen
         r_vinsch = 0.05*(l_vajer/(l_vajer + (s_last[i-1]*(1/3)))) # Funktion för vinschradiens förhållande till båtens position. (Förklaras i 'a' i rapport)
+
+
+        # Vi har lagt till ett brusfel med ett konstant fel på 0.1% plus ett slupmässigt mätfel mellan 0-0.1%.
+        U_motor[i] = (1 + 0.001 + (random.uniform(0, 1))*0.001)*U_motor[i] # brusfelet blir random mellan 0.1-0.2%
+        I_motor[i] = (1 + 0.001 + (random.uniform(0, 1))*0.001)*I_motor[i] # brusfelet blir random mellan 0.1-0.2%
         
         if(s_last[i-1] < 6 and v_ref > 0): # Båten är på trailern och åker ner mot vattnet
             v_ref = 0.0366
-            F_last[i] =(1 + (random.uniform(-1, 1))*0.01)*(F - F_f + F_a)
+            F_last[i] =(F - F_f + F_a)
 
         elif(s_last[i-1] >= 6 and s_last[i-1] <= 8 and v_ref >0): # Båten åker ut i vattnet i 2 meter
             v_ref= 0.0366
-            F_last[i] =(1 + (random.uniform(-1, 1))*0.01)*(F_a/math.cos(12))
+            F_last[i] =(F_a/math.cos(12))
 
         elif((s_last[i-1] > 8 and v_ref > 0) or (s_last[i-1] > 6 and v_ref < 0)): # Båten vänder riktning i vattnet och åker mot trailer
             
             v_ref = -0.0366
-            F_last[i] =(1 + (random.uniform(-1, 1))*0.01)*(F_a/math.cos(12))
+            F_last[i] =(F_a/math.cos(12))
 
         else: #Båten är på trailern och dras upp
-            F_last[i] =(1 + (random.uniform(-1, 1))*0.01)*(F + F_f + F_a)
+            F_last[i] =(F + F_f + F_a)
 
             if tryck > 100 or s_last[i-1] < 0.1: # [Pa] om det är 10 cm kvar på så bromsar den in
                 v_ref = 0
@@ -65,14 +70,8 @@ def main():
             # Om systemet vill få mer ström än vad elmotorn klarar av så stängs systemet av
             if abs(T_dev[i]/Ke) > maxI:
                 spärr = True
-            elif I_motor[i] > maxI:
-                I_motor[i] = maxI
-                w_motor[i] = (Ke * dt * U_motor[i] - dt * Ke * I_motor[i] * R + J * R * w_motor[i-1]) / (J * R + dt * Ke**2)
-                U_motor[i] = (w_motor[i]*Ke + R*I_motor[i])
-            else:
-                I_motor[i] = -maxI
-                w_motor[i] = (Ke * dt * U_motor[i] - dt * Ke * I_motor[i] * R + J * R * w_motor[i-1]) / (J * R + dt * Ke**2)
-                U_motor[i] = (w_motor[i]*Ke + R*I_motor[i])
+ 
+
         
         # Här tar vi ut spänningen och strömmen från systemet med sensorer
         # och sen använder vi dessa värden för att göra en virituell sensor för att hålla koll på hastigheten.
@@ -85,15 +84,20 @@ def main():
         # Här beräknas spänningen för nästa tidssteg med vår regulator
         if i < N-1:
             U_motor[i+1] = ((pid.update(v_last[i-1], v_ref)/r_vinsch)*k)*Ke + R*I_motor[i] 
-            if U_motor[i+1] > maxU:
-                U_motor[i+1] = maxU
-            elif U_motor[i+1] < -maxU:
-                U_motor[i+1] = -maxU
+
+            #Om förändringen i spänning blir för stor så önskar systemet mer ström än vad motorn klarar av
+            #men då ändras spänningen så att strömmen blir till sin max strm istället.
+            if U_motor[i+1] > (((Ke * dt * U_motor[i+1] - dt * Ke * maxI * R + J * R * w_motor[i]) / (J * R + dt * Ke**2))*Ke + R*maxI):
+                U_motor[i+1] = (((Ke * dt * U_motor[i+1] - dt * Ke * maxI * R + J * R * w_motor[i]) / (J * R + dt * Ke**2))*Ke + R*maxI)
+            
+            elif U_motor[i+1] < (((Ke * dt * U_motor[i+1] - dt * Ke * -maxI * R + J * R * w_motor[i]) / (J * R + dt * Ke**2))*Ke + R*-maxI):
+                U_motor[i+1] = (((Ke * dt * U_motor[i+1] - dt * Ke * -maxI * R + J * R * w_motor[i]) / (J * R + dt * Ke**2))*Ke + R*-maxI)
 
         if i > 0: #Eulers metod
             a_last[i] = (v_last[i] - v_last[i-1]) / dt
             s_last[i] = s_last[i-1] + dt*v_last[i]
     
+
     """
     Plotta grafer - hastighet, distans, acceleration, ström, spänning 
     """
